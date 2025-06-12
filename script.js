423628d87a02ba93cd02c0fac3c3c38a60034635
QUACK <suvan3664@gmail.com>
2025-06-13 02:47:42 +0530

    added button

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ alert("Welcome to the page!");
 
 // add a console log to the page
 console.log("This is a console log message.");
+
+// add a button to the page
+const button = document.createElement("button");
+button.textContent = "Click Me!";
+button.addEventListener("click", () => {
+    alert("Button was clicked!");
+});
+document.body.appendChild(button);
+// add a paragraph to the page
+const paragraph = document.createElement("p");
+paragraph.textContent = "This is a paragraph.";
+document.body.appendChild(paragraph);
+    
